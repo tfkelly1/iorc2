591d39c13ccd08be964eaebc395353bcb5b239b1
Tom Kelly <tfkelly1@gmail.com>
2012-12-05 21:33:59 -0600

    trying fix for heroku precompiled assets issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Iorc2::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  config.assets.precompile += %w( jquery.multiSelect.css )
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
