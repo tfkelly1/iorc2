e7820bc76131ab689810b2a5e40dee36c3f79c35
katherinekelly <katiekelly@eightbitstudios.com>
2023-02-24 14:13:29 -0600

    - meant to include regions_controller.rb in last commit - updated users_controller to use before_action, instead of before_filter; not sure why before_filter works in development mode locally...

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,3 +1,3 @@
 class RegionsController < ApplicationController
-    load_and_authorize_resource
+  load_and_authorize_resource
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,55 +1,51 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
+  before_action :authenticate_user!
 
-    def index
-      authorize! :index, @user, :message => 'Not authorized as an administrator.'
-      @users = User.all
-    end
+  def index
+    authorize! :index, @user, message: "Not authorized as an administrator."
+    @users = User.all
+  end
 
-    def show
-      @user = User.find(params[:id])
-    end
+  def show
+    @user = User.find(params[:id])
+  end
 
-    def update
-      authorize! :update, @user, :message => 'Not authorized as an administrator.'
-      @user = User.find(params[:id])
-      if @user.update_attributes(params[:user], :as => :admin)
-        redirect_to users_path, :notice => "User updated."
-      else
-        redirect_to users_path, :alert => "Unable to update user."
-      end
+  def update
+    authorize! :update, @user, message: "Not authorized as an administrator."
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user], as: :admin)
+      redirect_to users_path, notice: "User updated."
+    else
+      redirect_to users_path, alert: "Unable to update user."
     end
+  end
 
-    def destroy
-      authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
-      user = User.find(params[:id])
-      unless user == current_user
-        user.destroy
-        redirect_to users_path, :notice => "User deleted."
-      else
-        redirect_to users_path, :notice => "Can't delete yourself."
-      end
+  def destroy
+    authorize! :destroy, @user, message: "Not authorized as an administrator."
+    user = User.find(params[:id])
+    if user == current_user
+      redirect_to users_path, notice: "Can't delete yourself."
+    else
+      user.destroy
+      redirect_to users_path, notice: "User deleted."
     end
-    
-    def create
-      authorize! :create, @user, :message => 'Not authorized as an administrator.'
-      @user = User.find(params[:id])
-      if @user.save(params[:user], :as => :admin)
-        format.html { redirect_to :action => :index }
-      else
-        redirect_to users_path, :alert => "Unable to update user."
-      end
-    end
-    
-    def new
-      @user = User.new
-      respond_to do |format|
-        format.html # new.html.erb
-        format.json { render json: @user }
-      end
-    end
+  end
 
-  
-  
-  
+  def create
+    authorize! :create, @user, message: "Not authorized as an administrator."
+    @user = User.find(params[:id])
+    if @user.save(params[:user], as: :admin)
+      format.html { redirect_to action: :index }
+    else
+      redirect_to users_path, alert: "Unable to update user."
+    end
+  end
+
+  def new
+    @user = User.new
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @user }
+    end
+  end
 end
